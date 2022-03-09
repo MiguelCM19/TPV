de2627f62a33f8d712407ee4820f52a621b16502
MiguelCM19 <lesothosaurio@gmail.com>
2022-03-09 22:34:32 +0100

    comit

---
+++ src/conexion.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$database = "sexshop";
+$username = "root";
+$password = "";
+
+
+// Create connection
+$conexion = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conexion) {
+    die("Conexión fallida: " . mysqli_connect_error());
+}
+
+
+?>
---
+++ src/footer.php
@@ -0,0 +1,7 @@
+<footer class="fixed w-full bottom-0  bg-gray-900 text-center lg:text-left">
+      
+        <div class="text-white text-center p-4">Miguel Sex Shop © 2022 Copyright</div>
+    
+    </footer>
+</body>
+</html>
