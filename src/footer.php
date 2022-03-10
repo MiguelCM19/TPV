fe6aa20e6c308d3a77d918142e23a65c0a0d417c
MiguelCM19 <97629254+MiguelCM19@users.noreply.github.com>
2022-03-10 23:50:04 +0100

    Delete src directory

--- src/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="fixed w-full bottom-0  bg-gray-900 text-center lg:text-left">
-      
-        <div class="text-white text-center p-4">Miguel Sex Shop © 2022 Copyright</div>
-    
-    </footer>
-</body>
-</html>
