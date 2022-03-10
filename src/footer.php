38292c4deb107bd7dfc67094ec1b0df236ebf028
MiguelCM19 <lesothosaurio@gmail.com>
2022-03-10 23:53:16 +0100

    rr

--- src/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="fixed h-14 w-full bottom-0  bg-gray-900 text-clip lg:text-left mt-96">
-      
-        <div class="text-white text-center p-4 ">Miguel Sex Shop © 2022 Copyright</div>
-    
-    </footer>
-</body>
-</html>
